af9a7f87413aa5f75efd31d0efe4ee8ff86cb47e
mir1198yusuf <34382235+mir1198yusuf@users.noreply.github.com>
2020-10-08 01:00:42 +0530

    AccountNavigator - Display Login/MyAccount screen based on redux-store state

--- src/components/AccountNavigator.js
+++ src/components/AccountNavigator.js
@@ -1,26 +1,44 @@
 import React from "react";
-
 import {createStackNavigator} from "@react-navigation/stack";
-
 import LoginScreen from "./LoginScreen.js";
 import MyAccountScreen from "./MyAccountScreen.js";
+import {connect} from 'react-redux';
 
 const Stack = createStackNavigator();
 
 const AccountNavigator = (props) => {
 	
-	return (
-		<Stack.Navigator headerMode="screen"
-			screenOptions={{
-				headerTintColor : "white",
-				headerStyle : {
-					backgroundColor : "#443e3e",
-				},
-			}}	>
-			<Stack.Screen name="Login" component={LoginScreen}	/>
-			<Stack.Screen name="My Account" component={MyAccountScreen}	/>
-		</Stack.Navigator>
-	);
+	if (props.userReducer.USER) {
+		return (
+			<Stack.Navigator headerMode="screen"
+				screenOptions={{
+					headerTintColor : "white",
+					headerStyle : {
+						backgroundColor : "#443e3e",
+					},
+				}}	>
+				<Stack.Screen name="My Account" component={MyAccountScreen}	/>
+			</Stack.Navigator>
+		);
+	}
+	else {
+		return (
+			<Stack.Navigator headerMode="screen"
+				screenOptions={{
+					headerTintColor : "white",
+					headerStyle : {
+						backgroundColor : "#443e3e",
+					},
+				}}	>
+				<Stack.Screen name="Login" component={LoginScreen}	/>
+			</Stack.Navigator>
+		);
+	}
 }
 
-export default AccountNavigator;
+const mapStateToProps = state => {
+	const {userReducer} = state;
+	return {userReducer};
+};
+
+export default connect(mapStateToProps) (AccountNavigator);
